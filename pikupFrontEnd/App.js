88a3d3ed3d38746ab8ab6726578da48d8b453ad7
cfshor01 <cristobal.short@gmail.com>
2022-02-15 19:46:15 -0500

    adding base for event creation page

--- pikupFrontEnd/App.js
+++ pikupFrontEnd/App.js
@@ -1,111 +1,145 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+import axios from 'axios';
+import {
+  View,
+  useState,
+  useEffect,
+  Button,
+  Text,
+  StyleSheet,
+} from 'react-native';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  TextInput,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const burl = 'http://localhost:3000';
+// const url = 'https://reactnative.dev/movies.json';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App = () => {
+  const [isLoading, setLoading] = React.useState(true);
+  const [text, onChangeText] = React.useState(null);
+  const [eventDetails, onChangeDetails] = React.useState(null);
+  const [location, onChangeLocation] = React.useState(null);
+  const [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
+  const [isTimePickerVisible, setTimePickerVisibility] = React.useState(false);
+  const [participants, onChangeParticipants] = React.useState(null);
+  const [data, setData] = React.useState('');
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const callAPI = () => {
+    axios({
+      method: 'get',
+      url: `${burl}/users`,
+    }).then(response => {
+      console.log(response.status);
+      console.log(response.data);
+      setData(response.data.firstname);
+    });
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const postData = () => {
+    axios
+      .post(`${burl}/users`, {
+        id: text,
+        first_name: 'Cristobal',
+        last_name: 'Short',
+        email: 'cristobal.short@outook.com',
+      })
+      .then(response => {
+        setData(response.data[-1].event);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false);
+  };
+  const handleDateConfirm = date => {
+    console.warn('A date has been picked: ', date);
+    hideDatePicker();
+  };
+  const handleTimeConfirm = time => {
+    console.warn('A time has been picked: ', time);
+    hideTimePicker();
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Text style={styles.textStyle}>Event Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder="Enter Event Name"
+        value={text}
+      />
+      <Text style={styles.textStyle}>Details</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeDetails}
+        value={eventDetails}
+        placeholder="Event Details"
+      />
+      <Text style={styles.textStyle}>Location</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeLocation}
+        value={location}
+        placeholder="Event Location"
+      />
+      <Text style={styles.textStyle}>Time and Date</Text>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleDateConfirm}
+        onCancel={hideDatePicker}
+      />
+      <Button title="Show Time Picker" onPress={showTimePicker} />
+      <DateTimePickerModal
+        isVisible={isTimePickerVisible}
+        mode="time"
+        onConfirm={handleTimeConfirm}
+        onCancel={hideTimePicker}
+      />
+      <Text style={styles.textStyle}>Number of Participants Required</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeParticipants}
+        value={participants}
+        keyboardType="numeric"
+        placeholder="Participant #"
+      />
+      <Button title="Call API" onPress={postData}></Button>
+      <Text>API Val {text}</Text>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  textStyle: {
+    margin: 12,
   },
 });
 
